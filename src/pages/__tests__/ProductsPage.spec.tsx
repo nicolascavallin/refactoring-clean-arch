ff88e5cb1eeb19adf3743775dca299cb67975cea
Nicolas Cavallin <nicavallin@gmail.com>
2024-12-10 18:35:43 +0100

    add mock web server

--- src/pages/__tests__/ProductsPage.spec.tsx
+++ src/pages/__tests__/ProductsPage.spec.tsx
@@ -1,22 +1,47 @@
-import { expect, test } from "vitest";
+import { afterAll, afterEach, beforeAll, describe, expect, test } from "vitest";
 import { render, RenderResult, screen } from "@testing-library/react";
 import { ProductsPage } from "../ProductsPage";
 import { ReactNode } from "react";
 import { AppProvider } from "../../context/AppProvider";
+import { MockWebServer } from "../../test/MockWebServer";
+import ResponseJSON from "./data/response.json";
+
+const mockWebServer = new MockWebServer();
 
 function myCustomRender(component: ReactNode): RenderResult {
     return render(<AppProvider>{component}</AppProvider>);
 }
 
-test("Loads and displays title", () => {
-    myCustomRender(<ProductsPage />);
+function givenProducts() {
+    mockWebServer.addRequestHandlers([
+        {
+            method: "get",
+            endpoint: "https://fakestoreapi.com/products",
+            httpStatusCode: 200,
+            response: ResponseJSON,
+        },
+    ]);
+}
 
-    screen.getByRole("heading", { name: "Product price updater" });
-});
+describe("ProductsPage", () => {
+    beforeAll(() => mockWebServer.start());
+    afterEach(() => mockWebServer.resetHandlers());
+    afterAll(() => mockWebServer.close());
 
-test("Avoid fake positive", () => {
-    myCustomRender(<ProductsPage />);
+    test("Loads and displays title", () => {
+        givenProducts();
 
-    const heading = screen.queryByRole("heading", { name: "Product price updaterx" });
-    expect(heading).toBeNull();
+        myCustomRender(<ProductsPage />);
+
+        screen.getByRole("heading", { name: "Product price updater" });
+    });
+
+    test("Avoid fake positive", () => {
+        givenProducts();
+
+        myCustomRender(<ProductsPage />);
+
+        const heading = screen.queryByRole("heading", { name: "Product price updaterx" });
+        expect(heading).toBeNull();
+    });
 });

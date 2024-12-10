8169bcc2018b53788f317c82d19f4b7a82cf78ae
Nicolas Cavallin <nicavallin@gmail.com>
2024-12-10 18:06:24 +0100

    custom render function

--- src/pages/__tests__/ProductsPage.spec.tsx
+++ src/pages/__tests__/ProductsPage.spec.tsx
@@ -1,15 +1,21 @@
 import { expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
-import App from "../../App";
+import { render, RenderResult, screen } from "@testing-library/react";
+import { ProductsPage } from "../ProductsPage";
+import { ReactNode } from "react";
+import { AppProvider } from "../../context/AppProvider";
+
+function myCustomRender(component: ReactNode): RenderResult {
+    return render(<AppProvider>{component}</AppProvider>);
+}
 
 test("Loads and displays title", () => {
-    render(<App />);
+    myCustomRender(<ProductsPage />);
 
     screen.getByRole("heading", { name: "Product price updater" });
 });
 
 test("Avoid fake positive", () => {
-    render(<App />);
+    myCustomRender(<ProductsPage />);
 
     const heading = screen.queryByRole("heading", { name: "Product price updaterx" });
     expect(heading).toBeNull();
